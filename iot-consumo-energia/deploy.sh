866eb2675494a82fd51147e61657986692e7f117
Bruno Urbano Rodrigues <brunourb@gmail.com>
2024-02-18 10:34:03 -0300

    feat: inserido arquivo deploy.sh de exemplo

--- iot-consumo-energia/deploy.sh
+++ iot-consumo-energia/deploy.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "Compactando o projeto"
+zip iot-consumo-energia.zip lambda.py requirements.txt
+
+echo "Fazendo upload do arquivo"
+aws s3 cp iot-consumo-energia.zip s3://inserir-um-nome-unico-globalmente/iot-consumo-energia.zip --profile=aws_academy
+
+echo "Subindo a stack no cloudformation"
+aws cloudformation create-stack --profile=aws_academy --region=us-east-1 \
+    --stack-name 'iot-consumo-energia-stack' \
+    --capabilities CAPABILITY_IAM \
+    --template-body file://$(pwd)/stack.yaml
--- iot-consumo-energia/lambda.py
+++ iot-consumo-energia/lambda.py
@@ -1,4 +1,70 @@
+from datetime import datetime
+import json
+import logging
+import boto3
+#Estamos criando uma instância da classe Logger para 'melhorar' a forma de apresentar 'dados'
+#para o usuário fazer a 'auditoria' das informações que são processadas no lambda.
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
+#Tudo numa função LAMBDA começa pelo HANDLER.
+# O HANDLER é responsável para 'iniciar' o processamento de uma função lambda.
+# Geralmente em código python, podemos 'substituir' pelo método main(event, context)
+#O evento que contém a informação relacionada ao 'recurso' que está enviando a mensagem
+#Exemplo: uma SQS (fila), envia um JSON 'gigante' que está dentro da propriedade body
+"""
+Exemplo de um payload em SQS
+{
+  "Records": [
+    {
+      "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+      "receiptHandle": "MessageReceiptHandle",
+      "body": "{  
+                       \"Message\":\"   {  
+                            \\"ApplicationReference\\":2
+                        }\"
+                     }",
+      "attributes": {
+        "ApproximateReceiveCount": "1",
+        "SentTimestamp": "1523232000000",
+        "SenderId": "123456789012",
+        "ApproximateFirstReceiveTimestamp": "1523232000001"
+      },
+      "messageAttributes": {},
+      "md5OfBody": "7b270e59b47ff90a553787216d55d91d",
+      "eventSource": "aws:sqs",
+      "eventSourceARN": "arn:aws:sqs:eu-west-1:156445482446:TestQueue",
+      "awsRegion": "eu-west-1"
+    }
+  ]
+}
+"""
 def handler(event, context):
 
+    #event
+    #As informações que serão 'tratadas/recebidas' estão dentro da variável event
+
+    #context
+    #É o contexto/instância/domínio de serviços que o serviço
+    #  utilizador (lambda) pode acessar/identificar
+
+
+
+    #Nome da função com ARN (item identificador)
+    logger.info(f"Lambda function ARN: {context.invoked_function_arn}")
+    
+    #Nome do grupo de log que é exibid no cloudwatch
+    logger.info(f"CloudWatch log stream name: {context.log_stream_name}") 
+    logger.info(f"CloudWatch log group name: {context.log_group_name}")
+    
+    #ID da requisição do lambda
+    logger.info(f"Lambda Request ID: {context.aws_request_id}")
+    
+    #logger.info(f"Início processamento de dados do dispositivo nº {event['idDispositivo']}")
+    
+    #resultado = processar_dados(event, context)
+    
+    #logger.info(f"Fim processamento de dados do dispositivo nº {event['idDispositivo']}")    
+    #logger.debug(resultado)
+    
     return None
